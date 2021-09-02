1fd8a780dece9a279bd81ede0dab97d74fdbb820
hassansanto <86800434+hassansanto@users.noreply.github.com>
2021-09-02 21:16:25 +0600

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,23 @@
+
+<?php
+ob_start();
+//Include header.php File
+include('header.php')
+?>
+<?php
+/*Include _cart-template.php File */
+include('Template/_cart-template.php')
+/*Include _cart-template.php File */
+?>
+
+
+<?php
+/*Include _new-arts.php File */
+include('Template/_new-arts.php')
+/*Include _new-arts.php File */
+?>
+
+<?php
+//Include footer.php File
+include('footer.php')
+?>
