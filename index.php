92908d11a446ffac97eb002acc9d2eb47e8c0ade
hassansanto <86800434+hassansanto@users.noreply.github.com>
2021-09-02 21:16:50 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+ob_start();
+//Include header.php File
+include('header.php')
+?>
+
+<?php
+/*Include _banner-area.php File */
+include('Template/_banner-area.php')
+/*Include _banner-area.php File */
+?>
+
+<?php
+/*Include _top-sale.php File */
+include('Template/_top-sale.php')
+/*Include _top-sale.php File */
+?>
+
+<?php
+/*Include _special-price.php File */
+include('Template/_special-price.php')
+/*Include _special-price.php File */
+?>
+
+<?php
+/*Include _video-ads.phpFile */
+include('Template/_video-ads.php')
+/*Include _video-ads.php File */
+?>
+
+
+
+<?php
+/*Include _new-arts.php File */
+include('Template/_new-arts.php')
+/*Include _new-arts.php File */
+?>
+
+<?php
+/*Include _blogs.phpFile */
+include('Template/_blogs.php')
+/*Include _blogs.php File */
+?>
+
+<?php
+//Include footer.php File
+include('footer.php')
+?>
