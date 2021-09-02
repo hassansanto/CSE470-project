62c89815750d22e1edf9496e2b4fe80b70cddf11
hassansanto <86800434+hassansanto@users.noreply.github.com>
2021-09-02 21:02:06 +0600

    Add files via upload

---
+++ product.php
@@ -0,0 +1,22 @@
+
+<?php
+//Include header.php File
+include('header.php')
+?>
+<?php
+/*Include _products.php File */
+include('Template/_products.php')
+/*Include _products.php File */
+?>
+
+
+<?php
+/*Include _top-sale.php File */
+include('Template/_top-sale.php')
+/*Include _top-sale.php File */
+?>
+
+<?php
+//Include footer.php File
+include('footer.php')
+?>
